17eee3fef2732626ddb5b6a8e71e9c5a4487d66b
Ope Animash <opeanimashaun76@gmail.com>
2025-02-05 20:32:06 -0600

    new CheckoutForm for return URL to match hat is in render

--- client/src/components/CheckoutForm.jsx
+++ client/src/components/CheckoutForm.jsx
@@ -8,6 +8,7 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
+
 export default function CheckoutForm({emptyCart}) {
   const stripe = useStripe();
   const elements = useElements();
@@ -31,7 +32,7 @@ export default function CheckoutForm({emptyCart}) {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://cozy-threads-front-end-service.onrender.com/payment-success`,
+        return_url: `${window.location.origin}/payment-success`,
       },
     });
 
